413aea8366ed3fe95f00775dcc98830927e52317
Julia Herron <julia.herron@gmail.com>
2016-01-05 10:09:38 -0800

    let's try recursive

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,13 +1,22 @@
 # deep duplicate
 # goal: create totally new object with new object_id's for the outer object as well every single the inner one
 
-arr = [ [1, 2], [3, 4] ]
+deepest = [4]
+arr = [ [1, 2], [3, deepest] ]
+
 
 
 class Array
 
   def deep_dup
     puts "#{self.object_id}-Original"
+    new_array = []
+    # base case
+    if ! nest.is_a? Array 
+      return new_array
+    else
+      self.each do |nest| 
+
     # target sub-arrays
     self.each do |first_nesting|
         puts "#{first_nesting} - first_nest - #{first_nesting.object_id}"
@@ -15,6 +24,12 @@ class Array
         puts "#{duped_array} - Duped - #{duped_array.object_id}"
     end
 
+    self.each do |first_nesting|
+      puts first_nesting.object_id
+      first_nesting.each do |deepest|
+        puts deepest.object_id if deepest.is_a? Array
+      end
+    end
     # duplicate sub-arrays
     # stitch sub-arrays together
     # return new shiny duped array
@@ -36,4 +51,5 @@ class Array
 
 end
 
+puts "#{deepest} - deepest nest - #{deepest.object_id}"
 arr.deep_dup
