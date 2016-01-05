b2d7af29838059b7e59d1260ffa568c70bfa4ca7
Julia Herron <julia.herron@gmail.com>
2016-01-05 11:25:30 -0800

    tweeter class

--- Tweeter.rb
+++ Tweeter.rb
@@ -14,12 +14,21 @@ class Tweeter
     @tweets << message[0..143]
   end
 
-  def each
-    
+  def each(proc=nil)
+    index = 0
+    while index < @tweets.length
+      block_given? ? yield(@tweets[index]) : proc.call(@tweets[index])
+      index += 1    
+    end
   end
 
 end
 
-tweet = Tweeter.new
 
-print tweet.tweet("Something or other")
+t = Tweeter.new
+
+t.tweet("Something or other")
+t.tweet("This is my second tweet")
+
+t.each {|msg| puts msg}
+t.each {|msg| puts msg.upcase}
